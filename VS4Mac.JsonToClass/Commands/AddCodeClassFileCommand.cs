818b4419a5d2f88151b24c94f88ea33a9fa2ea01
LuisM000 <luis.m.r@outlook.com>
2019-02-03 15:21:02 +0100

    Json to class added in context menu

---
+++ VS4Mac.JsonToClass/Commands/AddCodeClassFileCommand.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using MonoDevelop.Components.Commands;
+using MonoDevelop.Core;
+using MonoDevelop.Ide;
+using MonoDevelop.Ide.Gui.Pads.ProjectPad;
+using VS4Mac.JsonToClass.Extensions;
+using VS4Mac.JsonToClass.Services;
+using Xwt;
+
+namespace VS4Mac.JsonToClass.Commands
+{
+    public class AddCodeClassFileCommand : CommandHandler
+    {
+        const string ClassOutputName = "Empty.cs";
+
+        protected override void Run()
+        {
+            var projectFolder = (ProjectFolder)IdeApp.ProjectOperations.CurrentSelectedItem;
+            string json = Clipboard.GetText();
+
+            var filePath = new FilePath(Path.Combine(projectFolder.Path, ClassOutputName));
+            string classCode = new JsonToClassService().GenerateClassCodeFromJson(json);
+
+            IdeApp.ProjectOperations.CurrentSelectedProject.CreateAndAddFileToProject(filePath, classCode);
+        }
+
+        protected override void Update(CommandInfo info)
+        {
+            info.Visible = IdeApp.Workspace.IsOpen &&
+                !string.IsNullOrEmpty(Clipboard.GetText()) &&
+                IdeApp.ProjectOperations.CurrentSelectedItem is ProjectFolder;
+        }
+
+    }
+}
---
+++ VS4Mac.JsonToClass/Extensions/ProjectOperationsExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using MonoDevelop.Core;
+using MonoDevelop.Ide;
+using MonoDevelop.Ide.Gui;
+using MonoDevelop.Projects;
+
+namespace VS4Mac.JsonToClass.Extensions
+{
+    public static class ProjectOperationsExtension
+    {
+        public static void CreateAndAddFileToProject(this Project project, FilePath filePath, string content)
+        {
+            File.WriteAllText(filePath, content);
+            IdeApp.ProjectOperations.AddFilesToProject(project, new FilePath[] { filePath }, filePath.ParentDirectory);
+        }
+    }
+}
